f44498588a218a90845b6b7086063b2c97789a46
Guilherme <guilhermengel1@gmail.com>
2025-04-28 17:42:42 -0300

    Adicionando Dados da pizza finalizado

--- lib/pizza_data.dart
+++ lib/pizza_data.dart
@@ -19,4 +19,9 @@ Pizza ({
 
 List<Pizza> pizzaData = [
   Pizza(nome: 'Pizza Marguerita', ingredientes: 'Tomate e mussarela', nomeFoto: 'margherita.jpg', preco: 10, soldOut: false),
+  Pizza(nome: 'Pizza Funghi', ingredientes: 'Tomate, mussarela, cogumelos e cebola', nomeFoto: 'funghi.jpg', preco: 12, soldOut: false),
+  Pizza(nome: 'Pizza Foccacia', ingredientes: 'Pão com azeite italiano e alecrim', nomeFoto: 'focaccia.jpg', preco: 8, soldOut: false),
+  Pizza(nome: 'Pizza Prosciutto', ingredientes: 'Tomate, mussarela, presunto, rúcula e queijo burrata', nomeFoto: 'prosciutto.jpg', preco: 14, soldOut: false),
+  Pizza(nome: 'Pizza Salamino', ingredientes: 'Tomate, mussarela e calabresa', nomeFoto: 'salamino.jpg', preco: 16, soldOut: true),
+  Pizza(nome: 'Pizza Spinaci', ingredientes: 'Tomate, mussarela, espinafre e ricota', nomeFoto: 'spinaci.jpg', preco: 12, soldOut: false),
 ];
